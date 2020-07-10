fde84c056559986e705a520cef777c2bf771597c
Matthew Szurkowski <61432155+MatthewSzurkowski@users.noreply.github.com>
2020-07-10 12:47:56 -0400

    Add files via upload

---
+++ Multiply strings.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        number1 = num1
+        number2 = num2
+        number1 = int(number1)
+        number2 = int(number2)
+        number3 = number1 * number2
+        number3 = str(number3)
+        return number3
